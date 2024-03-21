2ab71a0bfa1db0fdf414602ca0dd47c0bb11764f
Joao <joaovitoras146@gmail.com>
2024-03-20 22:02:36 -0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-
-        menu.imprimirMenu();
-
-        menu.addUsuario(new Usuario("Marco", "10", "masculino", "Silva", "Macarrão"));
-        menu.addUsuario(new Usuario("Sandra", "12", "feminino", "Aurélia", "Peixe"));
-        menu.addUsuario(new Usuario("Guilherminho", "5", "masculino", "Rocha", "Batata"));
+        System.out.println("hello");
     }
 
 }
