6921ce88e6f978bcbf3cc2af60389b666b4a599a
Joao <joaovitoras146@gmail.com>
2024-03-27 21:02:47 -0300

    feat: add menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Menu menu = new Menu();
+
+        menu.imprimirMenu();
+
+        menu.addUsuario(new Usuario("Marco", "10", "masculino", "Silva", "Macarrão"));
+        menu.addUsuario(new Usuario("Sandra", "12", "feminino", "Aurélia", "Peixe"));
+        menu.addUsuario(new Usuario("Guilherminho", "5", "masculino", "Rocha", "Batata"));
     }
 
 }
